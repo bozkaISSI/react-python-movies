aeedd1a484fbfef0a5038b7ffb6337ad6a0987a8
zewazla <zewazla@gmail.com>
2025-01-26 09:51:29 +0100

    feat: Add icons/pi

--- ui/src/ActorListItem.js
+++ ui/src/ActorListItem.js
@@ -1,14 +1,19 @@
+import { FaTrashAlt } from "react-icons/fa";
+import { PiFilmSlateFill } from "react-icons/pi";
+
 export default function ActorsListItem(props) {
     return (
         <div>
-            <div>
-                <span>{props.actor.name}</span>
-                {' '}
-                <strong>{props.actor.surname}</strong>
-                {' '}
-                <a onClick={props.onDelete}>Delete</a>
-            </div>
+            <div className='test'>
+                <div>
+                    <span>{props.actor.name}</span>
+                    {' '}
+                    <strong>{props.actor.surname}</strong>
+                    {' '}
+                </div>
 
+                <div className='button-delete' onClick={props.onDelete}><FaTrashAlt /></div>
+            </div>
         </div>
     );
 }
--- ui/src/MovieListItem.js
+++ ui/src/MovieListItem.js
@@ -1,16 +1,24 @@
+import { FaTrashAlt } from "react-icons/fa";
+import { PiFilmSlateFill } from "react-icons/pi";
+
 export default function MovieListItem(props) {
     return (
         <div>
-            <div>
+            <div className='test'>
+                <div>
+                    <PiFilmSlateFill />
                 <strong>{props.movie.title}</strong>
                 {' '}
                 <span>({props.movie.year})</span>
                 {' '}
                 directed by {props.movie.director}
                 {' '}
-                <a onClick={props.onDelete}>Delete</a>
+                {props.movie.description}
+                </div>
+
+                <div className='button-delete' onClick={props.onDelete}><FaTrashAlt /></div>
             </div>
-            {props.movie.description}
+
         </div>
     );
 }
