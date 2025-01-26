6860de78d381fc71dddbe6b6d2ce2bc83b486d6d
zewazla <zewazla@gmail.com>
2025-01-26 14:10:43 +0100

    feat: Toast success after DELETE | movie | actor

--- ui/src/ActorList.js
+++ ui/src/ActorList.js
@@ -1,13 +1,19 @@
 import ActorsListItem from "./ActorListItem";
+import { toast } from "react-toastify";
 
 export default function ActorList({ actors, onDeleteActor }) {
+  const handleDeleteActor = (actor) => {
+    onDeleteActor(actor);
+    toast.success(`Actor "${actor.name} ${actor.surname}" deleted successfully!`);
+  };
+
   return (
     <div>
       <h2>Actors</h2>
       <div className="actor-list">
         {actors.map((actor) => (
           <div key={actor.id} className="actor-item">
-            <ActorsListItem actor={actor} onDelete={() => onDeleteActor(actor)} />
+            <ActorsListItem actor={actor} onDelete={() => handleDeleteActor(actor)} />
           </div>
         ))}
       </div>
--- ui/src/MoviesList.js
+++ ui/src/MoviesList.js
@@ -1,13 +1,19 @@
 import MovieListItem from "./MovieListItem";
+import { toast } from "react-toastify";
 
 export default function MoviesList({ movies, onDeleteMovie }) {
+  const handleDeleteMovie = (movie) => {
+    onDeleteMovie(movie);
+    toast.success(`Movie "${movie.title}" deleted successfully!`);
+  };
+
   return (
     <div>
       <h2>Movies</h2>
       <div className="movies-list">
         {movies.map((movie) => (
           <div key={movie.title} className="movie-item">
-            <MovieListItem movie={movie} onDelete={() => onDeleteMovie(movie)} />
+            <MovieListItem movie={movie} onDelete={() => handleDeleteMovie(movie)} />
           </div>
         ))}
       </div>
