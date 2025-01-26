2f134637424d5f39624864c16aca866ef386caed
zewazla <zewazla@gmail.com>
2025-01-26 12:33:11 +0100

    feat: Add confirmation dialog 🟩

--- ui/src/ActorListItem.js
+++ ui/src/ActorListItem.js
@@ -1,12 +1,31 @@
 import { FaTrashAlt } from "react-icons/fa";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import default styles
 
 export default function ActorsListItem({ actor, onDelete }) {
+  const handleDelete = () => {
+    confirmAlert({
+      title: "Confirm Delete",
+      message: `Are you sure you want to delete "${actor.name} ${actor.surname}"?`,
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => onDelete(),
+        },
+        {
+          label: "No",
+          onClick: () => {},
+        },
+      ],
+    });
+  };
+
   return (
     <div className="main-container">
       <div>
         <span>{actor.name}</span> <strong>{actor.surname}</strong>
       </div>
-      <div className="button-delete" onClick={onDelete}>
+      <div className="button-delete" onClick={handleDelete}>
         <FaTrashAlt />
       </div>
     </div>
--- ui/src/MovieListItem.js
+++ ui/src/MovieListItem.js
@@ -1,7 +1,26 @@
 import { FaTrashAlt } from "react-icons/fa";
 import { PiFilmSlateFill } from "react-icons/pi";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 export default function MovieListItem({ movie, onDelete }) {
+  const handleDelete = () => {
+    confirmAlert({
+      title: "Confirm Delete",
+      message: `Are you sure you want to delete "${movie.title}"?`,
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => onDelete(),
+        },
+        {
+          label: "No",
+          onClick: () => {},
+        },
+      ],
+    });
+  };
+
   return (
     <div className="main-container">
       <div className="movie">
@@ -11,7 +30,7 @@ export default function MovieListItem({ movie, onDelete }) {
           {movie.director} {movie.description}
         </div>
       </div>
-      <div className="button-delete" onClick={onDelete}>
+      <div className="button-delete" onClick={handleDelete}>
         <FaTrashAlt />
       </div>
     </div>
