6bca526953bb24093fc55ef4aca63d86a325a5a3
zewazla <zewazla@gmail.com>
2025-01-26 10:22:38 +0100

    chore: refactor | js list | css global

--- ui/src/ActorList.js
+++ ui/src/ActorList.js
@@ -1,28 +1,16 @@
 import ActorsListItem from "./ActorListItem";
 
-export default function ActorList(props) {
-    return (
-        <div>
-            <h2>Actors</h2>
-            <div className="actor-list">
-                {props.actors.map((actor) => (
-                    <div
-                        key={actor.id}
-                        className="actor-item"
-                        style={{
-                            marginBottom: "8px",
-                            border: "1px solid #ccc",
-                            padding: "8px",
-                            borderRadius: "4px",
-                        }}
-                    >
-                        <ActorsListItem
-                            actor={actor}
-                            onDelete={() => props.onDeleteActor(actor)}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+export default function ActorList({ actors, onDeleteActor }) {
+  return (
+    <div>
+      <h2>Actors</h2>
+      <div className="actor-list">
+        {actors.map((actor) => (
+          <div key={actor.id} className="actor-item">
+            <ActorsListItem actor={actor} onDelete={() => onDeleteActor(actor)} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- ui/src/MoviesList.js
+++ ui/src/MoviesList.js
@@ -1,28 +1,16 @@
 import MovieListItem from "./MovieListItem";
 
-export default function MoviesList(props) {
-    return (
-        <div>
-            <h2>Movies</h2>
-            <div className="movies-list">
-                {props.movies.map((movie) => (
-                    <div
-                        key={movie.title}
-                        className="movie-item"
-                        style={{
-                            marginBottom: "8px",
-                            border: "1px solid #ccc",
-                            padding: "8px",
-                            borderRadius: "4px",
-                        }}
-                    >
-                        <MovieListItem
-                            movie={movie}
-                            onDelete={() => props.onDeleteMovie(movie)}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+export default function MoviesList({ movies, onDeleteMovie }) {
+  return (
+    <div>
+      <h2>Movies</h2>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <div key={movie.title} className="movie-item">
+            <MovieListItem movie={movie} onDelete={() => onDeleteMovie(movie)} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
